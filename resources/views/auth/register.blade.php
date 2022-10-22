320e46ecb778dd0fb4a73f61e6ce86975f09cbe4
lisa <lisa.giacobazzi@edu.unife.it>
2022-10-22 21:10:39 +0200

    se Dio vuole

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,28 +40,6 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surame') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
-
-                                @error('surname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="admin" class="col-md-4 col-form-label text-md-right">{{ __('Admin') }}</label>
-                        
-                            <div class="col-md-6">
-                                <input id="admin" type="checkbox" id="vehicle1" name="admin" value="true">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
