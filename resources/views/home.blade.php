9ab3623546274500fcf8069d17c56fb1368fa19d
lisa <lisa.giacobazzi@edu.unife.it>
2023-01-19 11:37:04 +0100

    unseful stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -10,7 +12,7 @@
                 <div class="card-body">
                     <!-- Stampo le ricette nella home -->
                     @foreach ($recipes as $recipe)
-                        <h3>{{ $recipe->name_recipe }}</h3>
+                        <h3> <a href="{{ route('recipe.show', $recipe)}}" class="card-link">{{ $recipe->name_recipe }}</a> </h3>
                         <img src="{{ Storage::url($recipe->photo) }}" class="card-img-top"  alt="foto ricetta">
                         <p><a href="#">{{ $recipe->instruction }}</a></p>
                     @endforeach
