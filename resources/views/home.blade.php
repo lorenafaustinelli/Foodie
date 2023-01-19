fef83198170aae14a1692d954e00c671a8ce446f
lisa <lisa.giacobazzi@edu.unife.it>
2023-01-19 12:10:30 +0100

    provaprovaprova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,13 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            
+        <div class="col-md-8">            
             <div class="card">
                 <div class="card-header">{{ __('Your Time Line') }}</div>
                 <div class="card-body">
                     <!-- Stampo le ricette nella home -->
                     @foreach ($recipes as $recipe)
-                            <h3> {{ $recipe->name_recipe }} </h3>
+                            <h3> <a href="{{ route('recipe.show', $receipe)}}" class="card-link"> {{ $recipe->name_recipe }} </a></h3>
                             <img src="{{ Storage::url($recipe->photo) }}" class="card-img-top"  alt="foto ricetta">
                             <p><a href="#">{{ $recipe->instruction }}</a></p>
                     @endforeach
