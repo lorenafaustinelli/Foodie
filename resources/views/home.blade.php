f3816131b5866b94eba678d0e998d44659e53783
lorenafaustinelli <lorena.faustinelli@edu.unife.it>
2023-01-19 12:18:54 +0100

    risolto problema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-body">
                     <!-- Stampo le ricette nella home -->
                     @foreach ($recipes as $recipe)
-                            <h3> <a href="{{ route('recipe.show', $receipe)}}" class="card-link"> {{ $recipe->name_recipe }} </a></h3>
+                            <h3> <a href="{{ route('recipe.show', $recipe)}}" class="card-link"> {{ $recipe->name_recipe }} </a></h3>
                             <img src="{{ Storage::url($recipe->photo) }}" class="card-img-top"  alt="foto ricetta">
                             <p><a href="#">{{ $recipe->instruction }}</a></p>
                     @endforeach
