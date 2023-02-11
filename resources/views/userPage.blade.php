372e6dcf5c89d0ab4a1438cb3bcb9c450bce5e35
lisa <lisa.giacobazzi@edu.unife.it>
2023-02-11 10:18:33 +0100

    immagine profilo, salvo prima di migrare

--- resources/views/userPage.blade.php
+++ resources/views/userPage.blade.php
@@ -3,8 +3,22 @@
 @section('content') 
 
 <!--Icon-->
-<!--Breve descrizione-->
+<div class="container text-center">
+	<div class="row">
+		<div class="col-10">
+			<img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3" style="width: 150px;" alt="Avatar" />
+			<h5 class="mb-2"><strong>{{ Auth::user()->name}} {{Auth::user()->surname}}</strong></h5>
+			<p class="text-muted">Web designer <span class="badge bg-primary">PRO</span></p>
+		</div>
+		<div class="col-2"> <!-- Barra laterale -->
+			prova 
+		</div>
+	</div>
+</div>
+
+<!--Breve descrizione dell'utente-->
 <!--Informazioni sull'utente: nome, cognome, email, indirizzo-->
 <!--Possibilità di eliminare definitivamente il profilo-->
 <!--Conteggio delle ricette scritte-->
 <!--Numero delle ricette salvate da altri-->
+@endsection
